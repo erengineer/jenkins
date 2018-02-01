0ce0400874462e75ab02cb3f46216305ab8a887d
erengineer <32784628+erengineer@users.noreply.github.com>
2018-02-01 17:49:44 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-JENKINS_HOME=/home/$(whoami)/jenkins_home
+JENKINS_HOME=/home/neteng/jenkins_home
 mkdir $JENKINS_HOME
 chown -R 1000 $JENKINS_HOME
 
-ANSIBLE_INVENTORY=/home/$(whoami)/ansible
+ANSIBLE_INVENTORY=/home/neteng/ansible
 mkdir $ANSIBLE_INVENTORY
 chown -R 1000 $ANSIBLE_INVENTORY
 
@@ -13,7 +13,7 @@ docker build -t jenkins-ci .
 docker run -d --name jenkins \
     -p 8080:8080 -p 50000:50000 \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /home/$(whoami)/jenkins_home:/var/jenkins_home \
-    -v /home/$(whoami)/ansible:/etc/ansible \
+    -v /home/neteng/jenkins_home:/var/jenkins_home \
+    -v /home/neteng/ansible:/etc/ansible \
     --restart unless-stopped \
     jenkins-ci
